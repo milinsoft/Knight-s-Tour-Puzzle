c78d678fb84e5c6a0887ff91cdb8090201840ed7
parseltongist <>
2021-11-25 10:02:17 +0100

    Stage 2/6 working.

--- game.py
+++ game.py
@@ -3,9 +3,6 @@
 # array = [[(x, y) for x in range(1, 9)] for y in range(8, 0, -1)]
 
 
-
-
-
 def print_grid(array, wdth, hght):
     row_n = hght  # as len of row is # 8
     placeholder = len(str(wdth * hght))
@@ -27,60 +24,40 @@ def print_grid(array, wdth, hght):
 
 
 def main():
-    # def create_grid():
-    # write validation rules here
+
     board_dim = input("Enter your board dimensions: ")
     try:
-        # try list comprehension maybe?
-        width, height = [int(x) for x in board_dim.split()]
-
-        if any([width < 0, height < 0]):
-        # or just raise a ValueError?
-            print("Invalid position!")
-            return main()
-
+        width, height = [int(x) for x in board_dim.split() if int(x) >= 0]
     except ValueError:
         print("Invalid position!")
         return main()
-
-    # print(type(width))
-    # print(type(height))
-        # print("current width:", width, "current height: ", height)
-
-        # return width, height
-
-        # create negative case.
-    # width, height = create_grid()
-
-
-    # creating the array (better with class)
 
     placeholder = "_" * len(str(width * height))
     array = [[placeholder for x in range(width)] for _ in range(height)]
 
     def visit_cell():
-        pos = input("Enter the knight's starting position: ")
-        try:
-            # make -1 more precisely
-            x, y = pos.split()
-            x = int(x)
-            y = int(y)
-            if not all([0 < y < height + 1, 0 < x < width + 1]):
+            pos = input("Enter the knight's starting position: ")
+            try:
+                x, y = [int(x) for x in pos.split()]
+
+                # make -1 more precisely
+                if not all([0 < y < height + 1, 0 < x < width + 1]):
+                    print("Invalid position!")
+                    return visit_cell()
+                # -y as actual beginning of our array is on hte top and starts with 0, and if to start from the end (-1) there is no need subtract 1
+
+                """ positions should be marked as _X or __X (instead of X_ or _X_),"""
+                # array[-y][x-1] = array[-y][x-1][:-1] + "X"  # replacing the last underscore with "X"
+                array[-y][x-1] = (len(placeholder) - 1) * " " + "X"  # something like ___ or __ to "  X" or "_X"
+
+            except ValueError:
                 print("Invalid position!")
                 return visit_cell()
-            # -y as actual beginning of our array is on hte top and starts with 0, and if to start from the end (-1) there is no need subtract 1
-
-            """ positions should be marked as _X or __X (instead of X_ or _X_),"""
-            # array[-y][x-1] = array[-y][x-1][:-1] + "X"  # replacing the last underscore with "X"
-            array[-y][x-1] = (len(placeholder) - 1) * " " + "X"  # something like ___ or __ to "  X" or "_X"
-
-        except ValueError:
-            print("Invalid position!")
-            return visit_cell()
 
     visit_cell()
 
     print_grid(array, width, height)
+
 
 
 if __name__ == '__main__':
